f0152c1a9965dffc719f687e7e5e6d5f17aaee0d
rushcut <rushcut@gmail.com>
2014-11-16 03:53:26 +0900

    generate cookbook with Berkshelf

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'tomcat'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures tomcat'
+long_description 'Installs/Configures tomcat'
+version          '0.1.0'
